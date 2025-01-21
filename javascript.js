f25e4f782bab84617caf27bead14fcc207fa30d3
mohammad0592 <mohzey8@gmail.com>
2025-01-22 01:07:40 +0200

    delete button

--- javascript.js
+++ javascript.js
@@ -3,13 +3,13 @@ const inputNew=document.getElementById("input-new-task");
 const validation=document.getElementById("showmessage");
 const list =document.getElementById("list");
 const validationpopup=document.getElementById("inputValpopup");
-const arrayoftasks=[];
+let arrayoftasks = JSON.parse(localStorage.getItem('tasks')) || [];
 var count = 0;
 
 
 
 
-addNewTask.addEventListener("click",function(){
+addNewTask.addEventListener("click",function(e){
   const taskname= inputNew.value.trim();
  if(inputNew.value.trim()===""){
   return showmessage("Task cannot be empty.","red");
@@ -22,9 +22,9 @@ addNewTask.addEventListener("click",function(){
  }
  const task=new tasks(taskname,false,count++);
  
- arrayoftasks.push(task);
-
+ arrayoftasks.push({name:taskname,completed:false,id:count++});
  const li =document.createElement("li");
+ li.id="li"+task.id;
  const div =document.createElement("div");
 
  const paragraph=document.createElement("p");
@@ -34,14 +34,13 @@ addNewTask.addEventListener("click",function(){
 
 
 
-
-
+  
   const checkbox=document.createElement("input");
   checkbox.type="checkbox";
   div.appendChild(checkbox);
 
-
   checkbox.addEventListener("change", function() {
+    
   const p=document.getElementById("paragraph"+task.id);
   task.completed=!task.completed;
   if(task.completed){
@@ -53,27 +52,22 @@ addNewTask.addEventListener("click",function(){
   }
 }); 
 
-
-
-
-
-
+ 
 const edit=document.createElement("button");
 edit.innerHTML=' <i class="fa-solid fa-pen"></i>';
 edit.classList.add("iconpencil");
 div.appendChild(edit);
 
-
-edit.addEventListener("click",function(){
+edit.addEventListener("click",()=>{
+const editp=document.getElementById("paragraph"+task.id);
 const editmodal=document.getElementById("edit-modal");
 const editinput=document.getElementById("edit-task-input");
 const saveedit=document.getElementById("save-edit-btn");
 const canceledit=document.getElementById("cancel-edit-btn");
 editinput.value=task.name;
 editmodal.classList.remove("hidden");
-
+saveedit.addEventListener("click",(index)=>{
 const editp=document.getElementById("paragraph"+task.id);
-saveedit.addEventListener("click",function(){
 const newname=editinput.value.trim();
 if(editinput.value.trim()===""){
   return showmessagepopup("Task cannot be empty.","red");
@@ -84,13 +78,13 @@ if(editinput.value.trim()===""){
  else if(newname.length<5){
  return showmessagepopup("Task must be at least 5 characters long.","red");
  }
-
+ 
  editp.innerHTML=newname;
  task.name=newname;
  editmodal.classList.add("hidden");
-
+ 
 });
-canceledit.addEventListener("click",function (){
+canceledit.addEventListener("click",()=>{
   editmodal.classList.add("hidden");
 
 });
@@ -101,10 +95,33 @@ canceledit.addEventListener("click",function (){
 
 
 
+
+
+
+
 const trashdelete=document.createElement("button");
 trashdelete.innerHTML=' <i class="fa-solid fa-trash"></i>';
 trashdelete.classList.add("icontrash");
 div.appendChild(trashdelete);
+
+ trashdelete.addEventListener("click",function(){
+  const deletemodal=document.getElementById("delete-modal");
+  const confirmdelete=document.getElementById("confirm-btn");
+  const canceldelete=document.getElementById("cancel-delete-btn");
+  deletemodal.classList.remove("hidden");
+  
+
+  confirmdelete.addEventListener("click", function(){
+    const deleteli=document.getElementById("li"+task.id);
+    deleteli.classList.add("hidden");
+    deletemodal.classList.add("hidden");
+    arrayoftasks.splice(task);
+  });
+  canceldelete.addEventListener("click",function (){
+    deletemodal.classList.add("hidden");
+  });
+  
+  });
 
 
  li.appendChild(div);
@@ -113,23 +130,31 @@ div.appendChild(trashdelete);
 });
 
 
+
+
+
+
 function tasks(name,completed,id){
     this.name=name;
     this.completed=completed;
     this.id=id;
+
+    
 }
+
+
 
 function showmessage(text,color){
 validation.innerHTML=text;
 validation.style.color=color;
-setTimeout(function (){ validation.textContent = ""; }, 3000);
+setTimeout(function (){ validation.textContent = ""; }, 1500);
 
 }
 
 function showmessagepopup(text,color){
   validationpopup.innerHTML=text;
   validationpopup.style.color=color;
-  setTimeout(function (){ validation.textContent = ""; }, 3000);
+  setTimeout(function (){ validation.textContent = ""; }, 1500);
   
   }
 
